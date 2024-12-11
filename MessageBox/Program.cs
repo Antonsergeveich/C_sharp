68b280f4d29b12f79e65b5ec61fcebbf1e2c88dd
Persh_vx33@top-academy.ru <Persh_vx33@top-academy.ru>
2024-12-12 01:46:54 +0300

    VisualBasic

--- MessageBox/Program.cs
+++ MessageBox/Program.cs
@@ -1,9 +1,15 @@
-﻿using System.Windows.Forms;
+﻿using Microsoft.VisualBasic;
+using System.Windows.Forms;
 
 class DialogDemo
 {
     static void Main()
     {
-        MessageBox.Show("Продолжаем изучать C#");
+        //MessageBox.Show("Продолжаем изучать C#");
+        //MessageBox.Show("Всем привет", "Окно с названием", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+        /*string name = Interaction.InputBox("Как Вас зовут?", "Давайте познакомимся");
+        string txt = "Очень приятно, " + name + "!";
+        MessageBox.Show(txt, "Знакомство состоялось",MessageBoxButtons.OK,MessageBoxIcon.Information);*/
+
     }
 }
