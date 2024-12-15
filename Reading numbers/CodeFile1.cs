57cdddb7ef858e820dbf0be95debf48c5b9e1c44
Persh_vx33@top-academy.ru <Persh_vx33@top-academy.ru>
2024-12-15 21:15:49 +0300

    +

--- Reading numbers/CodeFile1.cs
+++ Reading numbers/CodeFile1.cs
@@ -2,3 +2,22 @@
 using Microsoft.VisualBasic;
 using System.Windows.Forms;
 
+class EnteringInteger
+{
+    static void Main()
+    {
+        //Текстовые переменные
+        string res, txt;
+        //Целочисленные переменные
+        int year = 2024, age, born;
+        //Отображение окна с полем ввода
+        res = Interaction.InputBox("В каком году Вы родились?","Год рождения");
+        //Преобразование текста в число
+        born = Int32.Parse(res);
+        //Вычисление возраста
+        age = year-born;
+        txt = "Тогда Вам " + age + " лет";
+        //Окно с сообщением
+        MessageBox.Show(txt,"Возраст");
+    }
+}
