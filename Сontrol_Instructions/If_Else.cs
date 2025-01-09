a1a2d936e1455435eefc4a05c6649195eba5d2be
Persh_vx33@top-academy.ru <Persh_vx33@top-academy.ru>
2025-01-09 15:13:57 +0300

    If_Else_DONE

--- Сontrol_Instructions/If_Else.cs
+++ Сontrol_Instructions/If_Else.cs
@@ -1,7 +1,8 @@
 ﻿//#define UsingIfDemo
 //#define AnotherIfDemo
+//#define NestedIfDemo
 
-
+using System;
 using System.Reflection;
 using System.Windows.Forms;
 using Microsoft.VisualBasic;
@@ -52,3 +53,35 @@ class AnotherIfDemo
 	}
 } 
 #endif
+#if NestedIfDemo
+class NestedIfDemo
+{
+	static void Main()
+	{
+		string txt;
+		Console.Write("Введите текст: ");
+		txt = Console.ReadLine();
+		if (txt != "")
+		{
+			Console.WriteLine("Спасибо, что ввели текст!");
+			if (txt.Length > 10) Console.WriteLine("Ого, как много букв!");
+			else Console.WriteLine("Ого, как мало букв!");
+		}
+		else Console.WriteLine("Жаль, что не ввели текст!");
+	}
+} 
+#endif
+class AnotherNestedIfDemo
+{
+	static void Main()
+	{
+		int number;
+		Console.WriteLine("Введите целое число: ");
+		number = Int32.Parse(Console.ReadLine());
+		if (number == 1) Console.WriteLine("Единица");
+		else if (number == 2) Console.WriteLine("Двойка");
+		else if (number == 3) Console.WriteLine("Тройка");
+		else if (number == 4) Console.WriteLine("Четвёрка");
+		else Console.WriteLine("Неизвестное число");
+	}
+}
