8577f914f408581d54846c307899c3601b165846
Persh_vx33@top-academy.ru <Persh_vx33@top-academy.ru>
2025-01-09 14:51:20 +0300

    Add project files.

---
+++ Сontrol_Instructions/If_Else.cs
@@ -0,0 +1,54 @@
+﻿//#define UsingIfDemo
+//#define AnotherIfDemo
+
+
+using System.Reflection;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+
+#if UsingIfDemo
+class UsingIfDemo
+{
+	static void Main()
+	{
+		MessageBoxIcon icon;
+		string msg, title, name;
+		name = Interaction.InputBox("Как Вас зовут?", "Знакомимся");
+		if (name == "")
+		{
+			icon = MessageBoxIcon.Error;
+			msg = "Очень жаль, что мы не познакомились!";
+			title = "Знакомство не состоялось";
+		}
+		else
+		{
+			icon = MessageBoxIcon.Information;
+			msg = "Очень приятно, " + name + "!";
+			title = "Знакомство состоялось";
+		}
+		MessageBox.Show(msg, title, MessageBoxButtons.OK, icon);
+	}
+} 
+#endif
+
+#if AnotherIfDemo
+class AnotherIfDemo
+{
+	static void Main()
+	{
+		MessageBoxIcon icon = MessageBoxIcon.Error;
+		string msg = "Очень жаль, что мы не познакомились!",
+			title = "Знакомство не состоялось",
+			name;
+		name = Interaction.InputBox("Как Вас зовут?", "Знакомимся");
+		if (name != "")
+		{
+			icon = MessageBoxIcon.Information;
+			msg = "Очень приятно, " + name + "!";
+			title = "Знакомство состоялось";
+		}
+		MessageBox.Show(msg, title, MessageBoxButtons.OK, icon);
+	}
+} 
+#endif
