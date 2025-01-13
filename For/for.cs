de297071019545d10dc89c38735e1964735cc030
Persh_vx33@top-academy.ru <Persh_vx33@top-academy.ru>
2025-01-13 21:15:50 +0300

    +

--- For/for.cs
+++ For/for.cs
@@ -1,4 +1,6 @@
 ﻿//#define ForDemo
+//#define ForDemoTwo
+#define ForDemoThree
 
 using System;
 using System.Windows.Forms;
@@ -18,4 +20,31 @@ class ForDemo
 		Console.WriteLine(s);
 	}
 } 
+#endif
+#if ForDemoTwo
+class ForDemoTwo
+{
+	static void Main()
+	{
+		int n = 10, s = 0;
+		Console.Write("Сумма 1+3+5+...+ {0} = ", 2 * n - 1);
+		for (int k = 1; k <= n; k++)
+		{
+			s += 2 * k - 1;
+		}
+		Console.WriteLine(s);
+	}
+} 
+#endif
+#if ForDemoThree
+class ForDemoThree
+{
+	static void Main()
+	{
+		int n = 10, k, s;
+		Console.Write("Сумма 1+3+5+..+{0} = ", 2 * n - 1);
+		for (k = 1, s = 0; k <= n; s += 2 * k - 1, k++) ;
+		Console.WriteLine(s);
+	}
+}
 #endif
