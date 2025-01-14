35028f8f95cd658ba1ee573bee1d1fe47e932da2
Persh_vx33@top-academy.ru <Persh_vx33@top-academy.ru>
2025-01-14 21:58:50 +0300

    TryCatch

---
+++ TryCatch/TryCatch.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+class TryCatchDemo
+{
+	static void Main()
+	{
+		MessageBox.Show("Выполняется программа!","Начало");
+		try
+		{
+			Double.Parse(Interaction.InputBox("Введите действительное число: ", "Число"));
+			MessageBox.Show("Да, это было число!","Число");
+		}
+		catch
+		{
+			MessageBox.Show("Надо было ввести число","Ошибка",MessageBoxButtons.OK,MessageBoxIcon.Error);
+		}
+		MessageBox.Show("Программа завершена!","Завершение");
+	}
+}
