106e2da696b9b253baab0ce21d2a94feec733ad3
Persh_vx33@top-academy.ru <Persh_vx33@top-academy.ru>
2025-02-28 22:18:02 +0300

    Arrays 1

---
+++ Arrays/Arrays.cs
@@ -0,0 +1,27 @@
+﻿//#define IntArraysDemo_1
+
+using System;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+
+#if IntArraysDemo_1
+class IntArraysDemo_1
+{
+	static void Main()
+	{
+		Console.WriteLine("Массив чисел которые при делении на три дают в остатке единицу: ");
+		//Создание массива из двенадцати чисел
+		int[] nums = new int[12];
+		//Перебор элементов массива
+		for (int k = 0; k < nums.Length; k++)
+		{
+			//Присваивание значения элементу массива
+			nums[k] = 3 * k + 1;
+			//Отображение значения элементов массива
+			Console.Write("| " + nums[k] + " ");
+		}
+		Console.WriteLine("|");
+	}
+} 
+#endif
