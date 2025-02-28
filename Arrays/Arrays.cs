9de5fb2ec1ca51041c8587cef86b93b2d2d000bc
Persh_vx33@top-academy.ru <Persh_vx33@top-academy.ru>
2025-02-28 22:34:29 +0300

    +

--- Arrays/Arrays.cs
+++ Arrays/Arrays.cs
@@ -1,12 +1,12 @@
-﻿//#define IntArraysDemo_1
+﻿//#define IntArraysDemo
 
 using System;
 using System.Windows.Forms;
 using Microsoft.VisualBasic;
 
 
-#if IntArraysDemo_1
-class IntArraysDemo_1
+#if IntArraysDemo
+class IntArraysDemo
 {
 	static void Main()
 	{
@@ -25,3 +25,32 @@ class IntArraysDemo_1
 	}
 } 
 #endif
+class CharArrayDemo
+{
+	static void Main()
+	{
+		//Объект для генерирования случайных чисел:
+		Random rnd = new Random();
+		//Размер массива и индексная переменная:
+		int size = 10, k;
+		//Создание символьного массива:
+		char[] symbs = new char[size];
+		//Отображение сообщения:
+		Console.WriteLine("Массив случайных символов: ");
+		//Заполнение массива случайными символами:
+		for (k = 0; k < symbs.Length; k++)
+		{
+			symbs[k] = (char)('A'+rnd.Next(26));
+			//Отображение значения элементов массива:
+			Console.Write("| " + symbs[k] + " ");
+		}
+		Console.WriteLine("|");
+		//Отображение сообщения:
+		Console.WriteLine("Массив в обратном порядке: ");
+		for(k = symbs.Length-1; k >= 0; k--)
+		{
+			Console.Write("| " + symbs[k] + " ");
+		}
+		Console.WriteLine("|");
+	}
+}
