9020ee92930578fe01710e0fd047dace1323b7c4
Persh_vx33@top-academy.ru <Persh_vx33@top-academy.ru>
2025-03-01 17:07:00 +0300

    +

--- Arrays/Arrays.cs
+++ Arrays/Arrays.cs
@@ -1,4 +1,6 @@
 ﻿//#define IntArraysDemo
+//#define CharArrayDemo
+#define InitArrayDemo
 
 using System;
 using System.Windows.Forms;
@@ -25,6 +27,7 @@ class IntArraysDemo
 	}
 } 
 #endif
+#if CharArrayDemo
 class CharArrayDemo
 {
 	static void Main()
@@ -40,17 +43,47 @@ class CharArrayDemo
 		//Заполнение массива случайными символами:
 		for (k = 0; k < symbs.Length; k++)
 		{
-			symbs[k] = (char)('A'+rnd.Next(26));
+			symbs[k] = (char)('A' + rnd.Next(26));
 			//Отображение значения элементов массива:
 			Console.Write("| " + symbs[k] + " ");
 		}
 		Console.WriteLine("|");
 		//Отображение сообщения:
 		Console.WriteLine("Массив в обратном порядке: ");
-		for(k = symbs.Length-1; k >= 0; k--)
+		for (k = symbs.Length - 1; k >= 0; k--)
 		{
 			Console.Write("| " + symbs[k] + " ");
 		}
 		Console.WriteLine("|");
 	}
+} 
+#endif
+#if InitArrayDemo
+class InitArrayDemo
+{
+	static void Main()
+	{
+		//Создание и инициализация массива
+		int[] nums = { 1, 3, 5, 7, 6, 5, 4 };
+		char[] symbs = new char[] { 'A', 'Z', 'B', 'Y' };
+		string[] txts = new string[3] { "один", "два", "три" };
+		//Отображение содержимого массивов
+		Console.WriteLine("Массив nums: ");
+		for (int k = 0; k < nums.Length; k++)
+		{
+			Console.Write(nums[k]+" ");
+		}
+		Console.WriteLine("\nМассив symbs: ");
+		for(int k = 0;k < symbs.Length; k++)
+		{
+			Console.Write(symbs[k]+" ");
+		}
+		Console.WriteLine("\nМассив txts: ");
+		for(int k =0;k < txts.Length; k++)
+		{
+			Console.Write(txts[k]+" ");
+		}
+		Console.WriteLine();
+	}
 }
+#endif
