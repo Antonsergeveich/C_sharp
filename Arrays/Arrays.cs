d88fdab46da34405929155713c358a71a8ea01f7
Persh_vx33@top-academy.ru <Persh_vx33@top-academy.ru>
2025-03-05 09:17:18 +0300

    MultiDimArrayDemo

--- Arrays/Arrays.cs
+++ Arrays/Arrays.cs
@@ -6,7 +6,9 @@
 //#define SortArrayDemo
 //#define ForeachDemo
 //#define TwoDimArrayDemo
-#define InitTwoDimArrayDemo
+//#define InitTwoDimArrayDemo
+//#define MultiDimArrayDemo
+
 
 using System;
 using System.Windows.Forms;
@@ -360,6 +362,44 @@ class InitTwoDimArrayDemo
 			}
 			//Переход к новой строке:
 			Console.WriteLine();
+		}
+	}
+}
+#endif
+#if MultiDimArrayDemo
+class MultiDimArrayDemo
+{
+	static void Main()
+	{
+		//Трёхмерный символьный массив:
+		char[,,] symbs = new char[4, 3, 5];
+		//Одномерный символьный массив:
+		char[] s = { 'б', 'Б', 'b', 'B' };
+		//Заполнение трёхмерного символьного массива и отображение
+		//значений его элементов.
+		//Цикл по первому индексу:
+		for(int i = 0; i < symbs.GetLength(0); i++) 
+		{
+			//Отображение сообщения:
+			Console.WriteLine(" Слой № {0}:", i+1);
+			//Цикл по второму индексу:
+			for(int j = 0; j < symbs.GetLength(1); j++)
+			{
+				//Цикл по третьему индексу:
+				for(int k = 0; k < symbs.GetLength(2); k++)
+				{
+					//Присваивание значения элементу массива:
+					symbs[i, j, k] = s[i];
+					//Изменение элемента в одномерном массиве:
+					s[i]++;
+					//Отображение значения элемента массива:
+					Console.Write(symbs[i, j, k]+" ");
+				}
+				//Переход к новой строке:
+				Console.WriteLine();
+			}
+			//Отображение "линии":
+			Console.WriteLine("-------------------------");
 		}
 	}
 }
