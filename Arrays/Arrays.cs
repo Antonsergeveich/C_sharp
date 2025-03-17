74bea641d6a17d2783d05d7be14b37f2544385d9
Persh_vx33@top-academy.ru <Persh_vx33@top-academy.ru>
2025-03-17 08:55:34 +0300

    object

--- Arrays/Arrays.cs
+++ Arrays/Arrays.cs
@@ -8,7 +8,9 @@
 //#define TwoDimArrayDemo
 //#define InitTwoDimArrayDemo
 //#define MultiDimArrayDemo
-#define AnotherTwoDimArrayDemo
+//#define AnotherTwoDimArrayDemo
+//#define AnotherTwoDimArrayDemo_2
+#define ObjectArrayDemo
 
 using System;
 using System.Windows.Forms;
@@ -460,6 +462,122 @@ class AnotherTwoDimArrayDemo
 			}
 			Console.WriteLine();
 		}
+	}
+}
+#endif
+#if AnotherTwoDimArrayDemo_2
+class AnotherTwoDimArrayDemo_2
+{
+	static void Main()
+	{
+		//char[][] symbs = new char[5][];
+		char[][] symbs = new char[6][];
+		//int[][] nums = { new int[] {1,2,3}, new int[] {4,5}, new int[] {6,7,8,9} };
+		int[][] nums = { new int[] { 1, 2, 3, 4 }, new int[] { 5, 6 }, new int[] { 7, 8, 9, 10, 11, 12 } };
+		//char s = 'A';
+		char s = 'A';
+		//for (int i = 0; i < symbs.Length; i++)
+		//{
+		//	symbs[i] = new char[i + 3];
+		//	for (int j = 0; j < symbs[i].Length; j++)
+		//	{
+		//		symbs[i][j] = s;
+		//		s++;
+		//	}
+		//}
+		for(int i = 0; i < symbs.Length; i++)
+		{
+			symbs[i] = new char[i + 3];
+			for(int j = 0; j < symbs[i].Length; j++)
+			{
+				symbs[i][j] = s;
+				s++;
+			}
+		}
+		//for (int i = 0;i < nums.Length; i++)
+		//{
+		//	for (int j = 0; j < nums[i].Length; j++)
+		//	{
+		//		Console.Write("{0,2}", nums[i][j]);
+		//	}
+		//	Console.WriteLine();
+		//}
+		for(int i = 0;i < nums.Length; i++)
+		{
+			for(int j = 0; j < nums[i].Length; j++)
+			{
+				Console.Write("{0,2}  ", nums[i][j]);
+			}
+			Console.WriteLine();
+		}
+		//Console.WriteLine("---------------------");
+		Console.WriteLine("---------------------");
+		//foreach (char[] q in symbs)
+		//{
+		//	foreach(char p in q)
+		//	{
+		//		Console.Write("{0,2}", p);
+		//	}
+		//	Console.WriteLine();
+		//}
+		foreach (char[] q in symbs)
+		{
+			foreach(char p in q)
+			{
+				Console.Write("{0,2}", p);
+			}
+			Console.WriteLine();
+		}
+	}
+}
+#endif
+#if ObjectArrayDemo
+class ObjectArrayDemo
+{
+	static void Main()
+	{
+		//Массив из трёх переменных типа object:
+		Object[] objs = new Object[3];
+		//Элементам массива присваиваются значения разных типов:
+		objs[0] = 123;				//Целое число;
+		objs[1] = 'A';				//Символ;
+		objs[2] = "Третий элемент"; //Текст;
+		Console.WriteLine("Создан такой массив: ");
+		//Проверка содержимого массива:
+		for(int k = 0; k < objs.Length; k++)
+		{
+			Console.WriteLine(k+": "+objs[k]);
+		}
+		//Новые значения элементов:
+		objs[0] = (int)objs[0]+111; //Целое число;
+		objs[1] = "Второй элемент"; //Текст;
+		objs[2] = 3.141592;         //Действительное число;
+		Console.WriteLine("После присваивания значений: ");
+		//Проверка содержимого массива:
+		for(int k = 0;k < objs.Length; k++)
+		{
+			Console.WriteLine(k +": " + objs[k]);
+		}
+		//Целочисленный массив:
+		int[] nums = {10,20,30};
+		//Переменная массива присваивается как значение элементу массива:
+		objs[2] = nums;
+		Console.WriteLine("Целочисленный массив: ");
+		//Отображение элементов целочисленного массива:
+		for(int i = 0; i < ((int[])objs[2]).Length; i++) 
+		{ 
+			Console.Write("{0,3}", ((int[])objs[2])[i]);
+		}
+		Console.WriteLine();
+		//Новое значение элемента в числовом массиве:
+		((int[])objs[2])[1] = 0;
+		Console.WriteLine("Ещё раз тот же массив: ");
+		//Отображение элементов целочисленного массива:
+		for(int i = 0; i < nums.Length; i++)
+		{
+			Console.Write("{0,3}", nums[i]);
+		}
+		Console.WriteLine();
 	}
 }
 #endif
