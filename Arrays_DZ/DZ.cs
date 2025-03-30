371cb28a15b0fc0a993b12d82dbf7b2247c80576
Persh_vx33@top-academy.ru <Persh_vx33@top-academy.ru>
2025-03-30 15:40:42 +0300

    +

--- Arrays_DZ/DZ.cs
+++ Arrays_DZ/DZ.cs
@@ -7,7 +7,7 @@
 //#define Arrays_4_6_BubbleSort
 //#define Arrays_4_6_CocktailSort
 //#define Arrays_4_6_InsertionSort
-#define Arrays_4_6_InsertionSort_2
+//#define Arrays_4_6_InsertionSort_2
 //#define Arrays_4_6_SelectionSort
 
 //#define Arrays_4_6_Merge_Sort
@@ -1951,13 +1951,41 @@ class Arrays_4_6_InsertionSort_2
 	//		array[j+1] = buffer;
 	//	}
 	//}
+	//private static void InsertionSort(int[] array)
+	//{
+	//	for (int i = 1; i < array.Length; i++)
+	//	{
+	//		int buffer = array[i];
+	//		int j = i - 1;
+	//		while (j >= 0 && array[j] > buffer)
+	//		{
+	//			array[j+1] = array[j];
+	//			j--;
+	//		}
+	//		array[j+1] = buffer;
+	//	}
+	//}
+	//private static void InsertionSort(int[] array)
+	//{
+	//	for(int i = 1; i < array.Length; i++)
+	//	{
+	//		int buffer = array[i];
+	//		int j = i - 1;
+	//		while (j >= 0 && array[j]>buffer)
+	//		{
+	//			array[j+1] = array[j];
+	//			j--;
+	//		}
+	//		array[j+1]=buffer;
+	//	}
+	//}
 	private static void InsertionSort(int[] array)
 	{
-		for (int i = 1; i < array.Length; i++)
+		for(int i = 1; i < array.Length; i++)
 		{
 			int buffer = array[i];
 			int j = i - 1;
-			while (j >= 0 && array[j] > buffer)
+			while (j >= 0 && array[j]>buffer)
 			{
 				array[j+1] = array[j];
 				j--;
@@ -2738,12 +2766,46 @@ class Arrays_4_6_SelectionSort
 	//		array[min] = buffer;
 	//	}
 	//}
+	//private static void SelectionSort(int[] array)
+	//{
+	//	for (int i = 0;i < array.Length; i++)
+	//	{
+	//		int min = i;
+	//		for (int j = i + 1; j < array.Length; j++)
+	//		{
+	//			if (array[min] > array[j])
+	//			{
+	//				min = j;
+	//			}
+	//		}
+	//		int buffer = array[i];
+	//		array[i] = array[min];
+	//		array[min] = buffer;
+	//	}
+	//}
+	//private static void SelectionSort(int[] array)
+	//{
+	//	for(int i = 0;i < array.Length; i++)
+	//	{
+	//		int min = i;
+	//		for(int j = i + 1; j < array.Length; j++)
+	//		{
+	//			if (array[min] > array[j])
+	//			{
+	//				min = j;
+	//			}
+	//		}
+	//		int buffer = array[i];
+	//		array[i] = array[min];
+	//		array[min] = buffer;
+	//	}
+	//}
 	private static void SelectionSort(int[] array)
 	{
-		for (int i = 0;i < array.Length; i++)
+		for(int i = 0;i < array.Length; i++)
 		{
 			int min = i;
-			for (int j = i + 1; j < array.Length; j++)
+			for(int j = i + 1; j < array.Length; j++)
 			{
 				if (array[min] > array[j])
 				{
