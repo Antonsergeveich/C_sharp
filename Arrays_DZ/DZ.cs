6e388066c15577530eb0f38f42e81379686c4e5e
Persh_vx33@top-academy.ru <Persh_vx33@top-academy.ru>
2025-04-18 10:17:29 +0300

    +

--- Arrays_DZ/DZ.cs
+++ Arrays_DZ/DZ.cs
@@ -6995,18 +6995,42 @@ class Array_4_6_QuickSort
 		}
 		Console.WriteLine();
 	}
+	//private static void QuickSort(int[] array, int first, int end)
+	//{
+	//	if (first <= end) 
+	//	{
+	//		int left = first;
+	//		int right = end;
+	//		int middle = array[(left + right) / 2]; 
+	//		do
+	//		{
+	//			while (array[left] < middle) left++; 
+	//			while (array[right] > middle) right--;
+	//			if (left <= right) 
+	//			{
+	//				int buffer = array[left];
+	//				array[left] = array[right];
+	//				array[right] = buffer;
+	//				left++;
+	//				right--;
+	//			}
+	//		} while (left < right);  
+	//		QuickSort(array, first, right); 
+	//		QuickSort(array, left, end);   
+	//	}
+	//}
 	private static void QuickSort(int[] array, int first, int end)
 	{
-		if (first <= end) 
+		if (first < end)
 		{
 			int left = first;
 			int right = end;
-			int middle = array[(left + right) / 2]; 
+			int middle = array[(left + right) / 2];
 			do
 			{
-				while (array[left] < middle) left++; 
+				while (array[left] < middle) left++;
 				while (array[right] > middle) right--;
-				if (left <= right) 
+				if (left <= right)
 				{
 					int buffer = array[left];
 					array[left] = array[right];
@@ -7014,9 +7038,9 @@ class Array_4_6_QuickSort
 					left++;
 					right--;
 				}
-			} while (left < right);  
-			QuickSort(array, first, right); 
-			QuickSort(array, left, end);   
+			} while (left < right);
+			QuickSort(array, first, right);
+			QuickSort(array, left, end);
 		}
 	}
 }
