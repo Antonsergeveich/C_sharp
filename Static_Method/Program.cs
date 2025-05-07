5138d31740d1419822190037d38ad98db42852c3
Persh_vx33@top-academy.ru <Persh_vx33@top-academy.ru>
2025-05-07 09:36:08 +0300

    +

--- Static_Method/Program.cs
+++ Static_Method/Program.cs
@@ -1,7 +1,8 @@
 ﻿//#define StatMethDemo
 //#define OverloadMethDemo
 //#define ArrayToMethDemo
-#define ArrayFromMethDemo
+//#define ArrayFromMethDemo
+#define ArgsDemo
 
 using System;
 #if StatMethDemo
@@ -354,4 +355,97 @@ class ArrayFromMethDemo
 	}
 }
 
+#endif
+#if ArgsDemo
+class ArgsDemo
+{
+	//Первый метод. Аргумент - целое число:
+	static void alpha(int n)
+	{
+		//Проверка значения аргумента:
+		Console.WriteLine("В методе alpha(). На входе: " + n);
+		//Попытка изменить значения аргумента:
+		n++;
+		//Проверка значения аргумента:
+		Console.WriteLine("В методе alpha(). На выходе: " + n);
+	}
+	//Второй метод. Аргумент - ссылка на массив:
+	static void bravo(int[] n)
+	{
+		//Проверка содержимого массива:
+		Console.WriteLine("В методе bravo(). На входе: " + ArrayToText(n));
+		//Перебор элементов массива:
+		for (int k = 0; k < n.Length; k++)
+		{
+			//Попытка изменить значение элемента массива:
+			n[k]++;
+		}
+		//Проверка содержимого массива:
+		Console.WriteLine("В методе bravo(). На выходе: " + ArrayToText(n));
+	}
+	//Третий метод. Аргумент - ссылка на массив:
+	static void charlie(int[] n)
+	{
+		//Проверка содержимого массива:
+		Console.WriteLine("В методе charlie(). На входе: " + ArrayToText(n));
+		//Создаётся новый массив:
+		int[] m = new int[n.Length];
+		//Перебор элементов в массиве:
+		for (int k = 0; k < n.Length; k++)
+		{
+			//Значение элемента массива:
+			m[k] = n[k] + 1;
+		}
+		//Попытка присвоить новое значение аргументу:
+		n = m;
+		//Проверка содержимого массива:
+		Console.WriteLine("В методе charlie(). На выходе: " + ArrayToText(n));
+	}
+	//Метод для преобразования массива в текст:
+	static string ArrayToText(int[] n)
+	{
+		//Текстовая переменная:
+		string res = "[" + n[0];
+		//Перебор элементов массива (кроме начального):
+		for (int k = 1; k < n.Length; k++)
+		{
+			//Дописывание текста в текстовую переменную:
+			res += "," + n[k];
+		}
+		//Дописывание текста в текстовую переменную:
+		res += "]";
+		//Результат метода - текстовая строка:
+		return res;
+	}
+	//Главный метод программы:
+	static void Main()
+	{
+		//Переменная для передачи аргументом методу:
+		int A = 100;
+		//Проверка значения переменной:
+		Console.WriteLine("До вызова метода alpha(): A = " + A);
+		//Вызов метода:
+		alpha(A);
+		//Проверка значения переменной:
+		Console.WriteLine("После вызова метода alpha(): A = " + A);
+		//Массив для передачи аргументом методу:
+		Console.WriteLine();
+		int[] B = { 1, 3, 5 };
+		//Проверка содержимого массива:
+		Console.WriteLine("До вызова метода bravo(): B = " + ArrayToText(B));
+		//Вызов метода:
+		bravo(B);
+		//Проверка содержимого массива:
+		Console.WriteLine("После вызова метода bravo(): B = " + ArrayToText(B));
+		Console.WriteLine();
+		//Массив для передачи аргументом методу:
+		int[] C = { 2, 4, 6 };
+		//Проверка содержимого массива:
+		Console.WriteLine("До вызова метода charlie(): C = " + ArrayToText(C));
+		//Вызов метода:
+		charlie(C);
+		//Проверка содержимого массива:
+		Console.WriteLine("После вызова метода charlie(): C = "+ArrayToText(C));
+	}
+}
 #endif
