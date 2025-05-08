302f7a10f4ecf548ebcafb9d380482773a0d9183
Persh_vx33@top-academy.ru <Persh_vx33@top-academy.ru>
2025-05-08 11:33:56 +0300

    +

--- Static_Method/Program.cs
+++ Static_Method/Program.cs
@@ -565,72 +565,360 @@ class UsingOutDemo
 class RecursionDemo
 {
 	//Метод для вычисления факториала числа:
+	//static int factorial(int n)
+	//{
+	//	if (n == 1) return 1;
+	//	else return n * factorial(n - 1);
+	//}
+	//static int factorial(int n)
+	//{
+	//	if (n == 1) return 1;
+	//	else return n * factorial(n - 1);
+	//}
+	//static int factorial(int n)
+	//{
+	//	if (n == 1) return 1;
+	//	else return n * factorial(n - 1);
+	//}
+	//static int factorial(int n)
+	//{
+	//	if (n == 1) return 1;
+	//	else return n * factorial(n - 1);
+	//}
+	//static int factorial (int n)
+	//{
+	//	if (n == 1) return 1;
+	//	else return n * factorial(n - 1);
+	//}
+	//static int factorial(int n)
+	//{
+	//	if (n == 1) return 1;
+	//	else return n * factorial(n - 1);
+	//}
+	//static int factorial (int n)
+	//{
+	//	if (n == 1) return 1;
+	//	else return n * factorial(n - 1);
+	//}
+	//static int factorial(int n)
+	//{
+	//	if (n == 1) return 1;
+	//	else return n * factorial(n - 1);
+	//}
+	//static int factorial(int n)
+	//{
+	//	if (n == 1) return 1;
+	//	else return n * factorial(n - 1);
+	//}
+	//static int factorial(int n)
+	//{
+	//	if (n == 1) return 1;
+	//	else return n * factorial(n - 1);
+	//}
+	//static int factorial (int n)
+	//{
+	//	if (n == 1) return 1;
+	//	else return n * factorial(n - 1);
+	//}
+	//static int factorial(int n)
+	//{
+	//	if (n == 1) return 1;
+	//	else return n * factorial(n - 1);
+	//}
+	//static int factorial(int n)
+	//{
+	//	if (n == 1) return 1;
+	//	else return n * factorial(n - 1);
+	//}
+	//static int factorial(int n)
+	//{
+	//	if (n == 1) return 1;
+	//	else return n * factorial(n - 1);
+	//}
+	//static int factorial(int n)
+	//{
+	//	if (n == 1) return 1;
+	//	else return n * factorial(n - 1);
+	//}
+	//static int factorial(int n)
+	//{
+	//	if (n == 1) return 1;
+	//	else return n * factorial(n - 1);
+	//}
+	//static int factorial(int n)
+	//{
+	//	if (n == 1) return 1;
+	//	else return n * factorial(n - 1);
+	//}
+	//static int factorial(int n)
+	//{
+	//	if (n == 1) return 1;
+	//	else return n * factorial(n - 1);
+	//}
 	static int factorial(int n)
 	{
 		if (n == 1) return 1;
 		else return n * factorial(n - 1);
 	}
 	//Метод для вычисления чисел Фибоначчи:
-	static int fibs(int n)
+	//static int fibs(int n)
+	//{
+	//	if (n == 1 || n == 2) return 1;
+	//	else return fibs(n - 1) + fibs(n - 2);
+	//}
+	//static int fibonacci(int n)
+	//{
+	//	if (n == 1 || n == 2) return 1;
+	//	else return fibonacci(n - 1) + fibonacci(n - 2);
+	//}
+	//static int fibonacci(int n)
+	//{
+	//	if (n == 1 || n == 2) return 1;
+	//	else return fibonacci(n - 1) + fibonacci(n - 2);
+	//}
+	//static int fibonacci(int n)
+	//{
+	//	if (n == 1 || n == 2) return 1;
+	//	else return fibonacci(n - 1) + fibonacci(n - 2);
+	//}
+	//static int fibonacci(int n)
+	//{
+	//	if (n == 1 || n == 2) return 1;
+	//	else return fibonacci(n - 1) + (n - 2);
+	//}
+	//static int fibonacci(int n)
+	//{
+	//	if (n == 1 || n == 2) return 1;
+	//	else return fibonacci(n - 1) + fibonacci(n - 2);
+	//}
+	//static int fibonacci(int n)
+	//{
+	//	if (n == 1 || n == 2) return 1;
+	//	else return fibonacci(n - 1) + fibonacci(n - 2);
+	//}
+	//static int fibonacci(int n)
+	//{
+	//	if (n == 1 || n == 2) return 1;
+	//	else return fibonacci(n - 1) + fibonacci(n - 2);
+	//}
+	//static int fibonacci(int n)
+	//{
+	//	if (n == 1 || n == 2) return 1;
+	//	else return fibonacci(n - 1) + fibonacci(n - 2);
+	//}
+	//static int fibonacci(int n)
+	//{
+	//	if (n == 1 || n == 2) return 1;
+	//	else return fibonacci(n - 1) + fibonacci(n - 2);
+	//}
+	static int fibonacci(int n)
 	{
 		if (n == 1 || n == 2) return 1;
-		else return fibs(n - 1) + fibs(n - 2);
+		else return fibonacci(n - 1) + fibonacci(n - 2);
 	}
 	//Метод для вычисления суммы чисел:
+	//static int sum(int n)
+	//{
+	//	if (n == 0) return 0;
+	//	else return n + sum(n - 1);
+	//}
+	//static int sum(int n)
+	//{
+	//	if (n == 0) return 0;
+	//	else return n + sum(n - 1);
+	//}
+	//static int sum(int n)
+	//{
+	//	if (n == 0) return 0;
+	//	else return n + sum(n - 1);
+	//}
+	//static int sum(int n)
+	//{
+	//	if (n == 0) return 0;
+	//	else return n + sum(n - 1);
+	//}
+	//static int sum(int n)
+	//{
+	//	if (n == 0) return 0;
+	//	else return n + sum(n - 1);
+	//}
+	//static int sum(int n)
+	//{
+	//	if (n == 0) return 0;
+	//	else return n + sum(n - 1);
+	//}
+	//static int sum(int n)
+	//{
+	//	if (n == 0) return 0;
+	//	else return n + sum(n - 1);
+	//}
+	//static int sum(int n)
+	//{
+	//	if (n == 0) return 0;
+	//	else return n + sum(n - 1);
+	//}
+	//static int sum(int n)
+	//{
+	//	if (n == 0) return 0;
+	//	else return n + sum(n - 1);
+	//}
+	//static int sum(int n)
+	//{
+	//	if (n == 0) return 0;
+	//	else return n + sum(n - 1);
+	//}
+	//static int sum(int n)
+	//{
+	//	if (n == 0) return 0;
+	//	else return n + sum(n - 1);
+	//}
+	//static int sum(int n)
+	//{
+	//	if (n == 0) return 0;
+	//	else return n + sum(n - 1);
+	//}
+	//static int sum(int n)
+	//{
+	//	if (n == 0) return 0;
+	//	else return n + sum(n - 1);
+	//}
+	//static int sum(int n)
+	//{
+	//	if (n == 0) return 0;
+	//	else return n + sum(n - 1);
+	//}
+	//static int sum(int n)
+	//{
+	//	if (n == 0) return 0;
+	//	else return n + sum(n - 1);
+	//}
+	//static int sum(int n)
+	//{
+	//	if (n == 0) return 0;
+	//	else return n + sum(n - 1);
+	//}
+	//static int sum(int n)
+	//{
+	//	if (n == 0) return 0;
+	//	else return n + sum(n - 1);
+	//}
 	static int sum(int n)
 	{
 		if (n == 0) return 0;
 		else return n + sum(n - 1);
 	}
 	//Метод для отображения содержимого массива:
+	//static void show(int[] a, int k)
+	//{
+	//	//Отображения значения элемента массива:
+	//	Console.Write(a[k] + " ");
+	//	//Если элемент в массиве последний:
+	//	if (k == a.Length - 1)
+	//	{
+	//		Console.WriteLine();
+	//	}
+	//	//Если элемент в массиве не последний:
+	//	else
+	//	{
+	//		//Рекурсивный вызов метода:
+	//		show(a, k + 1);
+	//	}
+	//}
+	////Перегрузка метода для отображения содержимого массива:
+	//static void show(int[] a)
+	//{
+	//	//Вызов версии метода с двумя аргументами:
+	//	show(a, 0);
+	//}
+	//static void show(int[] a,int k)
+	//{
+	//	Console.Write(a[k] + " ");
+	//	if (k == a.Length - 1)
+	//	{
+	//		Console.WriteLine();
+	//	}
+	//	else
+	//	{
+	//		show(a, k + 1);
+	//	}
+	//}
+	//static void show(int[] a)
+	//{
+	//	show(a, 0);
+	//}
+	//static void show(int[] a, int k)
+	//{
+	//	Console.Write(a[k] + " ");
+	//	if(k == a.Length -1) Console.WriteLine();
+	//	else { show(a, k + 1); }
+	//}
+	//static void show(int[] a)
+	//{
+	//	show(a, 0);
+	//}
+	//static void show(int[] a, int k)
+	//{
+	//	Console.Write(a[k] + " ");
+	//	if (k == a.Length - 1) Console.WriteLine();
+	//	else show(a, k + 1);
+	//}
+	//static void show(int[] a) { show(a, 0); }
+	//static void show(int[] a, int k)
+	//{
+	//	Console.Write(a[k] + " ");
+	//	if (k == a.Length - 1) Console.WriteLine();
+	//	else { show(a, k + 1); }
+	//}
+	//static void show(int[] a) { show(a, 0); }
+	//static void show(int[] a, int k)
+	//{
+	//	Console.Write(a[k] + " ");
+	//	if (k == a.Length - 1) Console.WriteLine();
+	//	else
+	//	{
+	//		show(a, k + 1);
+	//	}
+	//}
+	//static void show(int[] a)
+	//{
+	//	show(a, 0);
+	//}
 	static void show(int[] a, int k)
 	{
-		//Отображения значения элемента массива:
 		Console.Write(a[k] + " ");
-		//Если элемент в массиве последний:
-		if (k == a.Length - 1)
-		{
-			Console.WriteLine();
-		}
-		//Если элемент в массиве не последний:
-		else
-		{
-			//Рекурсивный вызов метода:
-			show(a, k + 1);
-		}
+		if (k == a.Length - 1) Console.WriteLine();
+		else { show(a, k + 1); }
 	}
-	//Перегрузка метода для отображения содержимого массива:
 	static void show(int[] a)
 	{
-		//Вызов версии метода с двумя аргументами:
 		show(a, 0);
 	}
-	//Главный метод программы:
-	static void Main()
-	{
-		Console.WriteLine("Факториал числа:");
-		for (int k = 1; k <= 10; k++)
+		//Главный метод программы:
+		static void Main()
 		{
-			//Вычисления факториала числа:
-			Console.WriteLine(k + "!=" + factorial(k));
+			Console.WriteLine("Факториал числа:");
+			for (int k = 1; k <= 10; k++)
+			{
+				//Вычисления факториала числа:
+				Console.WriteLine(k + "!=" + factorial(k));
+			}
+			Console.WriteLine("Числа Фибоначчи: ");
+			for (int k = 1; k <= 10; k++)
+			{
+				//Вычисление чисел Фибоначчи:
+				Console.Write(fibonacci(k) + " ");
+			}
+			Console.WriteLine();
+			Console.Write("Сумма чисел от 1 до 100: ");
+			Console.WriteLine(sum(100));
+			//Числовой массив:
+			int[] A = { 1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21 };
+			Console.WriteLine("Числовой массив: ");
+			//Отображение всех элементов массива:
+			show(A);
+			Console.WriteLine("Элементы, начиная с третьего:");
+			//Отображение элементов начиная с третьего:
+			show(A, 2);
 		}
-		Console.WriteLine("Числа Фибоначчи: ");
-		for(int k = 1;k <= 10; k++)
-		{
-			//Вычисление чисел Фибоначчи:
-			Console.Write(fibs(k) + " ");
-		}
-		Console.WriteLine();
-		Console.Write("Сумма чисел от 1 до 100: ");
-		Console.WriteLine(sum(100));
-		//Числовой массив:
-		int[] A = { 1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21 };
-		Console.WriteLine("Числовой массив: ");
-		//Отображение всех элементов массива:
-		show(A);
-		Console.WriteLine("Элементы, начиная с третьего:");
-		//Отображение элементов начиная с третьего:
-		show(A, 2);
 	}
-}
 #endif
