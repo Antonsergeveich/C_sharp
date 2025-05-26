b176447466c1e6efa54d267ce2aba1b2d391cde2
Persh_vx33@top-academy.ru <Persh_vx33@top-academy.ru>
2025-05-26 21:50:58 +0300

    +

--- String/Program.cs
+++ String/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿//#define StringDemo
+#define IndexingStringDemo
+using System;
 
+#if StringDemo
 class StringDemo
 {
 	static void Main()
@@ -41,7 +44,7 @@ class StringDemo
 		//Локальная текстовая переменная:
 		String txt = "";
 		//Перебор элементов в массиве:
-		for(int k = 0; k < symbs.Length; k++)
+		for (int k = 0; k < symbs.Length; k++)
 		{
 			//Дописывание символа к текстовой строке:
 			txt += symbs[k];
@@ -55,7 +58,7 @@ class StringDemo
 		//Локальная текстовая переменная:
 		String txt = "";
 		//Локальная символьная переменная:
-		char s = a;	
+		char s = a;
 		//Формирование текстовой строки:
 		while (s <= b)
 		{
@@ -67,4 +70,70 @@ class StringDemo
 		//Результат метода:
 		return txt;
 	}
+} 
+#endif
+#if IndexingStringDemo
+class IndexingStringDemo
+{
+	static void Main()
+	{
+		//Отображение текстового литерала с помощью оператора цикла по коллекции:
+		foreach (char s in "Текст")
+		{
+			Console.Write(s + " ");
+		}
+		//Переход к новой строке:
+		Console.WriteLine();
+		//Отображение текстового литерала с помощью оператора цикла:
+		for (int k = 0; k < "Текст".Length; k++)
+		{
+			Console.Write("Текст"[k] + "_");
+		}
+		//Переход к новой строке:
+		Console.WriteLine();
+		//Текстовая переменная:
+		String A = "Изучаем C#";
+		//Отображение текстового значения:
+		for (int k = 0; k < A.Length; k++)
+		{
+			Console.Write(A[k]);
+		}
+		//Переход к новой строке:
+		Console.WriteLine();
+		//Отображение текста в обратном порядке:
+		showReversed(A);
+		//Проверка значения текстового объекта:
+		Console.WriteLine(A);
+		//Новый текстовый объект(текст в обратном порядке):
+		String B = getReversed(A);
+		//Отображение текстового значения:
+		Console.WriteLine(B);
+	}
+	//Статический метод для создания текстового объекта, в котором текст записан в обратном порядке:
+	static String getReversed(String txt)
+	{
+		//Локальная текстовая переменная:
+		String str = "";
+		//Формирование текстовой строки:
+		foreach(char s in txt)
+		{
+			//Добавление символа в начало строки:
+			str = s + str;
+		}
+		//Результат метода:
+		return str;
+	}
+	//Статический метод для отображения текста в обратном порядке:
+	static void showReversed(String txt)
+	{
+		//Перебор символов в тексте в обратном порядке:
+		for(int k = txt.Length - 1; k >= 0; k--)
+		{
+			//Отображение в консоли символа:
+			Console.Write(txt[k]);
+		}
+		//Переход к новой строке:
+		Console.WriteLine();
+	}
 }
+#endif
