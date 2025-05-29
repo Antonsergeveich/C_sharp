c41c845cd90444da11fe6a5f4dcb39b4fb2e5108
Persh_vx33@top-academy.ru <Persh_vx33@top-academy.ru>
2025-05-29 21:56:18 +0300

    +

--- String/Program.cs
+++ String/Program.cs
@@ -1,7 +1,7 @@
 ﻿//#define StringDemo
 //#define IndexingStringDemo
-#define CompStringDemo
-
+//#define CompStringDemo
+#define SearchStringDemo
 using System;
 
 #if StringDemo
@@ -271,6 +271,99 @@ class CompStringDemo
 		Console.WriteLine("StrCmp(A,C): {0}", StrCmp(A, C));
 		Console.WriteLine("B!=C: {0}", B != C);
 		Console.WriteLine("StrCmp(A, \"C#\"): {0}", StrCmp(A, "C#"));
+	}
+}
+#endif
+#if SearchStringDemo
+class SearchStringDemo
+{
+	static void Main()
+	{
+		//Текст в котором выполняется поиск:
+		String txt = "Итак, два плюс два и умножить на два равно шести";
+		Console.WriteLine("Исходный текст:");
+		Console.WriteLine("\"{0}\"", txt);
+		//Текстовая строка для поиска:
+		String str = "два";
+		//Отображение текстового значения:
+		Console.WriteLine("Строка для поиска:");
+		Console.WriteLine("\"{0}\"", str);
+		//Переменая для записи значения индекса:
+		int index;
+		//Индекс первого вхождения строки в текст:
+		index = txt.IndexOf(str);
+		Console.WriteLine("Первое совпадение: {0}", index);
+		//Индекс второго вхождения строки в текст:
+		index = txt.IndexOf(str, index + 1);
+		Console.WriteLine("Второе совпадение: {0}", index);
+		//Индекс последнего вхождения строки в текст:
+		index = txt.LastIndexOf(str);
+		Console.WriteLine("Посленее совпадение: {0}", index);
+		//Индекс для начала поиска и количество символов:
+		int a = 7, b = 9;
+		Console.WriteLine("Поиск с {0}-го индекса по {1} символам:", a, b);
+		//Индекс первого вхождения строки на интервале:
+		index = txt.IndexOf(str, a, b);
+		Console.WriteLine("Совпадение на индексе: {0}", index);
+		//Изменяем количество символов для поиска:
+		b += 3;
+		Console.WriteLine("Поиск с {0} - го индекса по {1} символам:", a, b);
+		index = txt.IndexOf(str, a, b);
+		Console.WriteLine("Совпадение на индексе: {0}", index);
+		//Символ для поиска:
+		char symb = 'а';
+		Console.WriteLine("Теперь ищем букву \'{0}\'", symb);
+		//Поиск первого совпадения:
+		index = txt.IndexOf(symb);
+		Console.WriteLine("Первое совпадение: {0}", index);
+		//Поиск второго совпадения:
+		index = txt.IndexOf(symb, index + 1);
+		Console.WriteLine("Второе совпадение: {0}", index);
+		//Последнее совпадение:
+		index = txt.LastIndexOf(symb);
+		Console.WriteLine("Последнее совпадение: {0}", index);
+		//Предпоследнее совпадение:
+		index = txt.LastIndexOf(symb, index - 1);
+		Console.WriteLine("Предпоследнее совпадение: {0}", index);
+		//Поиск на интервале:
+		index = txt.IndexOf(symb, a, b);
+		Console.WriteLine("Поиск на интервале индексов от {0} до {1}", a, a + b - 1);
+		Console.WriteLine("Первое совпадение на интервале: {0}", index);
+		//Последнее совпадение на интервале:
+		index = txt.LastIndexOf(symb, b, b + 1);
+		Console.WriteLine("Поиск до индекса {0}", b);
+		Console.WriteLine("Последнее совпадение на интервале: {0}", index);
+		//Новый символ для поиска:
+		symb = 'ы';
+		Console.WriteLine("Ищем букву \'{0}\'", symb);
+		//Поиск первого совпадения:
+		index = txt.IndexOf(symb);
+		Console.WriteLine("Первое совпадение: {0}", index);
+		//Массив с буквами для поиска:
+		char[] s = { 'ы', 'а', 'д' };
+		//Отображение символов для поиска:
+		Console.Write("Ищем букву \'{0}\'", s[0]);
+		for (int k = 1; k < s.Length - 1; k++)
+		{
+			Console.Write(", \'" + s[k] + "\'");
+		}
+		Console.WriteLine(" или \'" + s[s.Length - 1] + "\':");
+		//Первое совпадение:
+		index = txt.IndexOfAny(s);
+		Console.WriteLine("Первое совпадение: {0}", index);
+		//Второе совпадение:
+		index = txt.IndexOfAny(s, index + 1);
+		Console.WriteLine("Второе совпадение: {0}", index);
+		//Последнее совпадение:
+		index = txt.LastIndexOfAny(s);
+		Console.WriteLine("Последнее совпадение: {0}", index);
+		Console.WriteLine("Поиск на интервале индексов от {0} до {1}", a, a + b - 1);
+		//Первое совпадение на интервале:
+		index = txt.IndexOfAny(s, a, b);
+		Console.WriteLine("Первое совпадение на интервале: {0}", index);
+		//Последнее совпадение на интервале:
+		index = txt.LastIndexOfAny(s, a + b - 1, b);
+		Console.WriteLine("Последнее совпадение на интервале: {0}", index);
 	}
 }
 #endif
