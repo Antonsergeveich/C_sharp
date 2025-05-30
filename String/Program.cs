6fb4bb243d91b702321ea284f4270777b090e59f
Persh_vx33@top-academy.ru <Persh_vx33@top-academy.ru>
2025-05-30 10:55:06 +0300

    +

--- String/Program.cs
+++ String/Program.cs
@@ -1,8 +1,10 @@
 ﻿//#define StringDemo
 //#define IndexingStringDemo
 //#define CompStringDemo
-#define SearchStringDemo
+//#define SearchStringDemo
+#define ReplaceStringDemo
 using System;
+using System.Data.SqlTypes;
 
 #if StringDemo
 class StringDemo
@@ -364,6 +366,42 @@ class SearchStringDemo
 		//Последнее совпадение на интервале:
 		index = txt.LastIndexOfAny(s, a + b - 1, b);
 		Console.WriteLine("Последнее совпадение на интервале: {0}", index);
+	}
+}
+#endif
+#if ReplaceStringDemo
+class ReplaceStringDemo
+{
+	static void Main()
+	{
+		//Исходный текст:
+		String txt = "Мы изучаем C#";
+		//Проверка исходного текста:
+		Console.WriteLine(txt);
+		//Текстовая переменная:
+		String str;
+		//Вставка слова в текст:
+		str = txt.Insert(11, "язык ");
+		//Проверка текста:
+		Console.WriteLine(str);
+		//Вставка в текст слова с последующим замещением другого текстового блока:
+		str = str.Insert(3, "не ").Replace("C#", "Java");
+		//Проверка текста:
+		Console.WriteLine(str);
+		//Замена пробелов на подчёркивания:
+		str = str.Replace(" ", "_");
+		//Проверка текста:
+		Console.WriteLine(str);
+		//Извлечение подстроки:
+		str = str.Substring(2, 12);
+		//Проверка текста:
+		Console.WriteLine(str);
+		//Извлечение подстроки:
+		str = txt.Substring(3);
+		//Проверка текста:
+		Console.WriteLine(str);
+		//Проверка исходного текста:
+		Console.WriteLine(txt);
 	}
 }
 #endif
