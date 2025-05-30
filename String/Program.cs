476998d3cfe919ad118e7689b627ba727a78f11b
Persh_vx33@top-academy.ru <Persh_vx33@top-academy.ru>
2025-05-30 11:24:26 +0300

    +

--- String/Program.cs
+++ String/Program.cs
@@ -2,9 +2,12 @@
 //#define IndexingStringDemo
 //#define CompStringDemo
 //#define SearchStringDemo
-#define ReplaceStringDemo
+//#define ReplaceStringDemo
+#define SplittingStringDemo
+
 using System;
 using System.Data.SqlTypes;
+using System.Diagnostics.SymbolStore;
 
 #if StringDemo
 class StringDemo
@@ -402,6 +405,65 @@ class ReplaceStringDemo
 		Console.WriteLine(str);
 		//Проверка исходного текста:
 		Console.WriteLine(txt);
+	}
+}
+#endif
+#if SplittingStringDemo
+class SplittingStringDemo
+{
+	static void Main()
+	{
+		//Исходная текстовая строка:
+		String txt = "Дорогу осилит идущий";
+		//Отображение исходного текста:
+		Console.WriteLine(txt);
+		//Текстовая строка:
+		String str;
+		//Текст с символами в верхнем регистре:
+		str = txt.ToUpper();
+		//Проверка текстового значения:
+		Console.WriteLine(str);
+		//Текст с символами в нижнем регистре:
+		str = txt.ToLower();
+		//Проверка текстового значения:
+		Console.WriteLine(str);
+		//Пременная для текстового массива:
+		//String[] words;
+		////Разбивка текста на подстроки:
+		//words = txt.Split();
+		////Отображение подстрок:
+		//for (int k = 0; k < words.Length; k++)
+		//{
+		//	Console.WriteLine((k + 1) + ": " + words[k]);
+		//}
+		//Console.WriteLine();
+		////Разбивка текста на подстроки:
+		//words = txt.Split('у', 'и');
+		////Отображение подстрок:
+		//for (int k = 0; k < words.Length; k++)
+		//{
+		//	Console.WriteLine((k + 1) + ": " + words[k]);
+		//}
+		//Console.WriteLine();
+		////Разбивка текста на подстроки:
+		//words = txt.Split(new char[] { 'у', 'и' }, 3);
+		////Отображение подстрок:
+		//for (int k = 0; k < words.Length; k++)
+		//{
+		//	Console.WriteLine((k + 1) + ": " + words[k]);
+		//}
+		////Переменная для символьного массива:
+		//char[] symbs;
+		////Массив из символов, формирующих текст:
+		//symbs = txt.ToCharArray();
+		////Отображение символов из массива:
+		//for (int k = 0; k < symbs.Length; k++)
+		//{
+		//	Console.Write(symbs[k] + " ");
+		//}
+		String words;
+
+		Console.WriteLine();
 	}
 }
 #endif
